9720576869e6a2ca05f2a3f63f66da5cd084957f
indisid963 <119930460+indisid963@users.noreply.github.com>
2023-12-17 00:33:44 +0300

    FallenPostmanEchoTest.java

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(text));
+                .body("data", equalTo("text"));
     }
 }
